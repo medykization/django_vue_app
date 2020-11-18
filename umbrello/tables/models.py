6ab5095e1f338933acc59f7b3eba03fce0501e79
Leonard <medyk170@gmail.com>
2020-11-18 19:19:50 +0100

    test

--- umbrello/tables/models.py
+++ umbrello/tables/models.py
@@ -3,4 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Board(models.Model):
+    id = models.IntegerField(primary_key = True) 
     name = models.CharField(max_length=30)
+    owner_id = models.ForeignKey(User,  on_delete=models.CASCADE)
