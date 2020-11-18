989586df50827dfc4589cad119c7be1a804f9ac0
michaaeu <mik3wwl@gmail.com>
2020-11-18 02:28:33 +0100

    Just cleaning/preparing project

---
+++ umbrello/src/components/HomePage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <img alt="Background" src="../assets/splash.jpg">
+    <LandingPage msg="Welcome to Umbrello"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import LandingPage from '@/components/Home.vue'
+
+export default {
+  name: 'Home',
+};
+</script>
+
+<style scoped>
+
+</style>
