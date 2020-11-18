0f551352de2733fa4d04b3998d957ea4a44e0a00
Leonard <medyk170@gmail.com>
2020-11-18 01:45:46 +0100

    create tables app

--- umbrello/umbrello/settings.py
+++ umbrello/umbrello/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'user_auth',
+    'tables',
 ]
 
 REST_FRAMEWORK = {
--- umbrello/umbrello/urls.py
+++ umbrello/umbrello/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("account/", include('user_auth.urls','user_auth')),
+    path("table/", include('tables.urls','tables')),
     path('admin/', admin.site.urls),
 ]
