65f4ba614e5db80c83fc86d38b93c24fc6c64e89
Michał <mik3wwl@gmail.com>
2020-12-04 12:01:30 +0100

    Login/Register forms ui updated

--- frontend/umbrello/src/views/SignUp.vue
+++ frontend/umbrello/src/views/SignUp.vue
@@ -1,57 +1,67 @@
 <template>
-  <v-container fluid>
-    <v-layout row wrap>
-      <v-flex xs12 sm6 offset-sm3 class="text-xs-center" mt-5>
-        <h1>Sign Up</h1>
-      </v-flex>
-      <v-flex xs12 sm6 offset-sm3 mt-3>
-        <form>
-          <v-layout column>
-            <v-flex>
-            <v-text-field
-                name="username"
-                label="Username"
-                id="username"
-                type="username"
-                v-model="username"
-                required></v-text-field>
+<v-container fill-height>
+    <v-row justify="center" align="center">
+        <v-col cols="12" sm="3">
+          <v-card
+            elevation="10"
+            align-center
+            outlined
+            block
+            shaped
+          >
+            <v-flex xs12 sm6 offset-sm3 class="text-xs-center" mt-5>
+              <h1>Sign Up</h1>
             </v-flex>
-            <v-flex>
-              <v-text-field
-                name="email"
-                label="Email"
-                id="email"
-                type="email"
-                v-model="email"
-                required></v-text-field>
+            <v-flex xs12 sm6 offset-sm3 mt-3>
+              <form>
+                <v-layout column>
+                  <v-flex>
+                  <v-text-field
+                      name="username"
+                      label="Username"
+                      id="username"
+                      type="username"
+                      v-model="username"
+                      required></v-text-field>
+                  </v-flex>
+                  <v-flex>
+                    <v-text-field
+                      name="email"
+                      label="Email"
+                      id="email"
+                      type="email"
+                      v-model="email"
+                      required></v-text-field>
+                  </v-flex>
+                  <v-flex>
+                    <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      type="password"
+                      v-model="password"
+                      required></v-text-field>
+                  </v-flex>
+                  <v-flex>
+                    <v-text-field
+                      name="confirmPassword"
+                      label="Confirm Password"
+                      id="confirmPassword"
+                      type="password"
+                      v-model="confirmed_password"
+                      required
+                      ></v-text-field>
+                  </v-flex>
+                  <v-flex class="text-xs-center" my-5>
+                    <v-btn @click="registerUser()" color="primary" type="submit">Sign Up</v-btn>
+                  </v-flex>
+                </v-layout>
+              </form>
             </v-flex>
-            <v-flex>
-              <v-text-field
-                name="password"
-                label="Password"
-                id="password"
-                type="password"
-                v-model="password"
-                required></v-text-field>
-            </v-flex>
-            <v-flex>
-              <v-text-field
-                name="confirmPassword"
-                label="Confirm Password"
-                id="confirmPassword"
-                type="password"
-                v-model="confirmed_password"
-                required
-                ></v-text-field>
-            </v-flex>
-            <v-flex class="text-xs-center" mt-5>
-              <v-btn @click="registerUser()" color="primary" type="submit">Sign Up</v-btn>
-            </v-flex>
-          </v-layout>
-        </form>
-      </v-flex>
-    </v-layout>
-  </v-container>
+          </v-card>
+        </v-col>
+    </v-row>
+</v-container>
 </template>
 
 <script>
